1104a18b6c29c24ab3c6fb5ef592d8ecea00d401
Danyal <36112962+Danyal7865@users.noreply.github.com>
2018-02-10 11:48:48 -0500

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,52 @@
+print("What meme are you?")
+yeet = 0
+boi = 0
+spaghet = 0
+wae = 0
+
+print("1. Spagetti")
+print("2. Krabby Patty")
+print("3. Gold Rings")
+print("4. Tide Pods")
+answer = input("What's your favorite food?")
+if answer == "1": 
+    spaghet+=1
+if answer == "2":
+    boi+=1
+if answer == "3":
+    wae+=1
+if answer == "4":
+    yeet+=1
+    
+print("1. Purple")
+print("2. Red")
+print("3. Green")
+print("4. Yellow")
+answer = input("What's your favorite color?")
+if answer == "1":
+    yeet+=1
+if answer == "2":
+    wae+=1
+if answer == "3":
+    spaghet+=1
+if answer == "4":
+    boi+=1
+print("1. Patrick")
+print("2. Your Memes")
+print("3. Your Wife/Husband")
+print("4. Sanik")
+answer = input("Who would be your best friend?")
+if answer == "1":
+    boi+=1
+if answer == "2":
+    yeet+=1
+if answer == "3":
+    spaghet+=1
+if answer == "4":
+    wae+=1
+print("1.")
+print("2.")
+print("3.")
+print("4.")
+answer = input("")
+
